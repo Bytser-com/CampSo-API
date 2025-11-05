7a2a599e95a904bc160c03b0c1f3132e885dc7d2
RobbeKoremanPXL <robbe.koreman@student.pxl.be>
2025-11-05 09:23:21 +0100

    Created Spring maven project
    Created a java package structure com.bytser.campso.api and added main application class CampsoApiApplication.java
    Project is a maven spring boot project.
    Added dependicies I think will be needed for further development in pom.xml.
    Further improved / completed the pom.xml file with necessary details and information.

---
+++ src/main/java/com/bytser/campso/api/CampsoApiApplication.java
@@ -0,0 +1,13 @@
+package com.bytser.campso.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampsoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytser/campso/api/campso_api/CampsoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytser.campso.api.campso_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
