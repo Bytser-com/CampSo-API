5d901e52789c14392212005956f474e21b847e79
RobbeK <koremanrobbe@gmail.com>
2025-11-13 15:06:35 +0100

    Switch to PostgreSQL-backed repositories

--- src/main/java/com/bytser/campso/api/facilities/FacilityRepository.java
+++ src/main/java/com/bytser/campso/api/facilities/FacilityRepository.java
@@ -1,5 +1,13 @@
 package com.bytser.campso.api.facilities;
 
-public class FacilityRepository {
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacilityRepository extends JpaRepository<Facility, UUID> {
+
+    List<Facility> findByHostPlaceId(UUID hostPlaceId);
+
+    List<Facility> findByFacilityType(FacilityType facilityType);
 }
